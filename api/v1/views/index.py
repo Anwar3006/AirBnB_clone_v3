b2c5441317c6af11b5222397b53b405fa0a324ea
Anwar3006 <anwarsadat.d2@gmail.com>
2023-02-16 19:36:21 +0000

    Airbnb v3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,11 +3,13 @@ from api.v1.views import app_views
 from flask import jsonify
 import models
 
+
 @app_views.route('/status', strict_slashes=False)
 def status():
     """that returns a JSON of status code
     """
     return jsonify({"status": "OK"}), 200
+
 
 @app_views.route('/api/v1/stats', strict_slashes=False)
 def count():
